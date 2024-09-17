705eec6477a237c0d065d317bc2d90f88e1b1a24
karpsw <karpsw@DESKTOP-IJ83LVH>
2024-09-17 14:49:48 +0300

    fix hosts

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -1,10 +1,10 @@
 export function getImageUrl(src, width, height){
-    let url =  'https://astronews.ror.ru'+ src + '?w='+width+'&h='+height + '&q=80';
+    let url =  import.meta.env.PUBLIC_IMG_HOST+ src + '?w='+width+'&h='+height + '&q=80';
     return url;
 }
 
 export function getAbsoluteSiteUrl(href){
-    return import.meta.env.SITE_URL+ href;
+    return import.meta.env.PUBLIC_SITE_HOST+ href;
 }
 
 
