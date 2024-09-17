2f70fa37bef9f4b079ae46f22a2f688f350da98b
karpsw <karpsw@DESKTOP-IJ83LVH>
2024-09-17 13:04:31 +0300

    fix docker

--- docker/nginx/ng_reload.sh
+++ docker/nginx/ng_reload.sh
@@ -1,2 +1,18 @@
- docker cp nginx.conf astronews-nginx-purge-img:/etc/nginx/nginx.conf
- docker exec astronews-nginx-purge-img nginx -s reload
+#!/bin/sh -x
+PATH=$PATH:/bin:/usr/bin/:/usr/local/bin:/usr/sbin:/sbin
+DIRSRC=/var/apps/astronews/src
+
+df -h
+
+echo "load git fetch"
+cd $DIRSRC
+
+git reset --hard
+git fetch
+#git pull origin main
+git checkout main
+git pull origin main
+
+
+docker cp /var/apps/astronews/src/docker/nginx/nginx.conf astronews-nginx-purge-img:/etc/nginx/nginx.conf
+docker exec astronews-nginx-purge-img nginx -s reload
