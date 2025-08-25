92378fc5acf77a84429b99522a6070466ce8247b
waleric <v.darokhin@gmail.com>
2025-08-25 12:49:47 +0300

    исправил Promise

--- src/modules/post/getPostBlocks.ts
+++ src/modules/post/getPostBlocks.ts
@@ -1,6 +1,6 @@
 import type { PostBlocks } from '@/types/PostBlocks';
 
-export async function getPostBlocks(): Promise<HomeData> {
+export async function getPostBlocks(): Promise<PostBlocks> {
   const res = await fetch(`https://telegraf.news/api/v1/postBlocks.php`);
   if (!res.ok) throw new Error('Home not found');
   const raw = await res.json(); // вместо res.text()
